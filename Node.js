c71d5f5492cbbb2032d204ad9fd09b0099f0cc07
Alessandra747 <sandrapzr@hotmail.com>
2022-07-08 18:03:54 -0300

    Desafio1Mod2

--- Node.js
+++
@@ -1,9 +0,0 @@
-const nome = "Jujuba"
- console.log(nome)
-
- let numero = 6
- console.log(numero)
-
- let ano = 2016
-console.log(ano - numero)
-console.log(ano + numero)
---
+++ script.js
@@ -0,0 +1,34 @@
+// // Crie uma variável que receba um nome;
+
+const nome = "Maria";
+console.log(nome);
+
+// // Crie uma variável que receba um número;
+
+let numero = 4;
+
+console.log(numero);
+
+// // Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let ano = 1997;
+console.log(ano - numero);
+
+// // Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+console.log(ano + numero);
+
+// //Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log("Estou mexendo no console!");
+
+// // Crie uma variável chamada “quartaFeira” e atribua à ela
+// //o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = false;
+
+console.log(quartaFeira);
+
+// // Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof quartaFeira);
